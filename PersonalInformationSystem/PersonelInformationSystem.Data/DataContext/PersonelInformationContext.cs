dc7edaf610919c724ec85e4318c0ebe7f3fda95a
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-12-28 01:01:03 +0300

    add migration

--- PersonalInformationSystem/PersonelInformationSystem.Data/DataContext/PersonelInformationContext.cs
+++ PersonalInformationSystem/PersonelInformationSystem.Data/DataContext/PersonelInformationContext.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PersonelInformationSystem.Data.Models;
 
 namespace PersonelInformationSystem.Data.DataContext
 {
     public class PersonelInformationContext :IdentityDbContext
     {
-        public PersonelInformationContext(DbContextOptions options) :base(options)
-        {
+        public PersonelInformationContext(DbContextOptions options) : base(options)
+        {}
 
-        }
+        public DbSet<Personal> Personal { get; set; }
     }
 }
---
+++ PersonalInformationSystem/PersonelInformationSystem.Data/Models/Personal.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PersonelInformationSystem.Data.Models
+{
+    public class Personal : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TaxId { get; set; }
+        public DateTime DateOfBirth{ get; set; }
+
+
+    }
+}
