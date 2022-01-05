e4401ad7ff715a09f6cd4d9313351e6705a92ea2
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-01-06 00:33:35 +0300

    IPersonalLeaveTypesBusiness created

---
+++ PersonalInformationSystem/PersonalInformationSystem.UI/Controllers/PersonalLeaveTypesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalInformationSystem.Business.Contracts;
+
+namespace PersonalInformationSystem.UI.Controllers
+{
+    public class PersonalLeaveTypesController : Controller
+    {
+        private readonly IPersonalLeaveTypesBusiness _personalLeaveTypesBusiness;
+        public PersonalLeaveTypesController(IPersonalLeaveTypesBusiness personalLeaveTypesBusiness)
+        {
+            _personalLeaveTypesBusiness = personalLeaveTypesBusiness;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
