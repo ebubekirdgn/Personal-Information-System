d5cfa469f20c820310dec80b6b86c88d09cbe7eb
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-02-01 22:37:56 +0300

    update

--- PersonalInformationSystem/PersonalInformationSystem.Data/Contracts/IRepositoryBase.cs
+++ PersonalInformationSystem/PersonalInformationSystem.Data/Contracts/IRepositoryBase.cs
@@ -3,15 +3,10 @@
     public interface IRepositoryBase<T> where T : class, new()
     {
         IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null);
-
         T Get(int id);
-
         T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null);
-
         void Add(T entity);
-
         void Remove(T entity);
-
         void Update(T entity);
     }
 }
