41cccfce0743590e53bad2ecf0f00e61f89142ae
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-02-05 21:10:46 +0300

    update

--- PersonalInformationSystem/PersonalInformationSystem.Business/Contracts/IPersonalLeaveRequestBusiness.cs
+++ PersonalInformationSystem/PersonalInformationSystem.Business/Contracts/IPersonalLeaveRequestBusiness.cs
@@ -3,17 +3,11 @@
     public interface IPersonalLeaveRequestBusiness
     {
         Result<List<PersonalLeaveRequestVM>> GetAllLeaveRequestByUserId(string userId);
-
         Result<PersonalLeaveRequestVM> CreatePersonalLeaveRequest(PersonalLeaveRequestVM model, SessionContext user);
-
         Result<PersonalLeaveRequestVM> EditPersonalLeaveRequest(PersonalLeaveRequestVM model, SessionContext user);
-
         Result<PersonalLeaveRequestVM> GetAllLeaveRequestById(int id);
-
         Result<PersonalLeaveRequestVM> RemovePersonalRequest(int id);
-
         Result<List<PersonalLeaveRequestVM>> GetSendApprovedLeaveRequests();
-
         Result<bool> RejectPersonalLeaveRequest(int id);
     }
 }
