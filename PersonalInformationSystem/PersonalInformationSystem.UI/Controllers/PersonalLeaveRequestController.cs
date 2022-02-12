bca224e18db305b6a571f471f4fbb3c25f60d612
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-02-13 01:22:51 +0300

    create PersonalLeaveRequestController

---
+++ PersonalInformationSystem/PersonalInformationSystem.UI/Controllers/PersonalLeaveRequestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalInformationSystem.UI.Controllers
+{
+    public class PersonalLeaveRequestController : Controller
+    {
+
+        private readonly IPersonalLeaveRequestBusiness _personalLeaveRequestBusiness;
+        private readonly IPersonalLeaveTypesBusiness _personalLeaveTypesBusiness;
+
+        public PersonalLeaveRequestController(IPersonalLeaveRequestBusiness personalLeaveRequestBusiness,PersonalLeaveTypesBusiness personalLeaveTypesBusiness)
+        {
+            _personalLeaveRequestBusiness = personalLeaveRequestBusiness;
+            _personalLeaveTypesBusiness = personalLeaveTypesBusiness;
+        }
+
+
+        public IActionResult Index()
+        {
+            var user = JsonConvert.DeserializeObject<SessionContext>(HttpContext.Session.GetString(ResultConstant.LoginUserInfo));
+
+            var requestModel = _personalLeaveRequestBusiness.GetAllLeaveRequestByUserId(user.LoginId);
+            ViewBag.EmployeeLeaveTypes = _personalLeaveTypesBusiness.GetAllPersonalLeaveType();
+            if (requestModel.IsSuccess)
+                return View(requestModel.Data);
+            return View(user);
+        }
+    }
+}
