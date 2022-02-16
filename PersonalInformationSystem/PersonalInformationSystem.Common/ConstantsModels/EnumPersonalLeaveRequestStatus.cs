b24a8ce3ac2afbb6ee64346aa3ad5b71a9e1bfff
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-02-17 01:33:46 +0300

    create EnumPersonalLeaveRequestStatus

---
+++ PersonalInformationSystem/PersonalInformationSystem.Common/ConstantsModels/EnumPersonalLeaveRequestStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalInformationSystem.Common.ConstantsModels
+{
+    public enum EnumPersonalLeaveRequestStatus
+    {
+        [Display(Name = "Onaya Gönderildi")]
+        Send_Approved =1,
+
+        [Display(Name = "Onaylandı")]
+        Approved =2,
+
+        [Display(Name = "Reddedildi")]
+        Rejected =3
+    }
+}
