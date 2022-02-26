8a4a29e213ea61bfef6c9f5401044116bf9c2a56
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-02-27 02:45:36 +0300

    WorkOrder created

---
+++ PersonalInformationSystem/PersonalInformationSystem.Data/Models/WorkOrder.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonalInformationSystem.DataAccess.Models
+{
+    public class WorkOrder : BaseEntity
+    {
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        [MaxLength(750)]
+        public string WorkOrderDescription { get; set; }
+
+        public int WorkOrderStatus { get; set; }
+
+        public double WorkOrderPoint { get; set; }
+        public string PhotoPath { get; set; }
+
+        [MaxLength(35)]
+        public string WorkOrderNumber { get; set; }
+
+        public string AssignPersonalId { get; set; }
+        [ForeignKey("AssignPersonalId")]
+        public Personal AssignPersonal { get; set; }
+    }
+}
